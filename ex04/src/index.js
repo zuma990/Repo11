1f4b3648ab1345d4860b1c8af6e2a11745118f61
zuma990 <zumajuve@gmail.com>
2022-04-08 00:14:24 +0200

    Added new file

---
+++ ex04/src/index.js
@@ -0,0 +1,33 @@
+// Create a temps array here
+var temps = [
+    [31, 32, 19, 37],
+    [29, 27, 55, 36],
+    [17, 27, 42, 46],
+    [29, 52, 21, 64],
+    [91, 27, 31, 61] 
+];
+function myArrayFunction(arr){
+      var sum = 0;
+      var averageDayTemp = [];
+      
+    for (var i=0; i<arr.length; i++){
+        var total = 0;
+        
+        for (var j=0; j<arr[i].length; j++){
+            
+            total = total + arr[i][j];
+        }
+        sum = total / arr[i].length;
+        
+       averageDayTemp.push(sum);
+    } 
+    
+    return averageDayTemp;
+}
+console.log(myArrayFunction(temps));
+// End of temps array 
+// Create a function below this line
+
+// End of creating a function
+
+module.exports = myArrayFunction;
